51a616a9f30d80e49eab9ce09a1423777f6d4c03
xforker-sudo <chelker755@gmail.com>
2026-05-19 04:58:10 +0400

    upв

--- backend/src/marketplace/marketplace.module.ts
+++ backend/src/marketplace/marketplace.module.ts
@@ -4,11 +4,11 @@ import { ProductsService } from './products.service';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 import { PaymentsModule } from '../payments/payments.module';
+import { NotificationsModule } from '../notifications/notifications.module'; // <-- добавлено
 
 @Module({
+  imports: [PaymentsModule, NotificationsModule], // <-- добавлен NotificationsModule
   controllers: [ProductsController, OrdersController],
   providers: [ProductsService, OrdersService],
-  imports: [PaymentsModule],
-  
 })
 export class MarketplaceModule {}
